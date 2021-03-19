b8cb3dec78acf372090402517f0110afd9e59a2a
liangweiliang <liangweiliang@coocaa.com>
2021-03-19 10:33:32 +0800

    feat:自动把3位的数字转成6位

--- src/loader.js
+++ src/loader.js
@@ -3,15 +3,26 @@ import stylus from 'stylus';
 import csstree from 'css-tree'
 import runtime from './runtime'
 
+const nodes = stylus.nodes
+const originRGBAToString = nodes.RGBA.prototype.toString
+nodes.RGBA.prototype.toString = function () {
+  // https://github.com/stylus/stylus/issues/1825
+  // six digit color
+  const val = originRGBAToString.call(this)
+  if (/^#[A-Za-z0-9]{3}/.test(val)) {
+    return '#' + val[1] + val[1] + val[2] + val[2] + val[3] + val[3]
+  }
+  return val
+}
+
 export default async function stylusLoader(source) {
-  const styl = stylus(source);
+  const styl = stylus(source)
   const callback = this.async();
   styl.render(async (error, css) => {
     if (error) {
       if (error.filename) {
         this.addDependency(path.normalize(error.filename));
       }
-
       callback(error);
     }
     const ast = csstree.parse(css);
--- test/example.js
+++ test/example.js
@@ -3,7 +3,7 @@ import { JsvTextStyleClass } from "jsview-utils/JsViewReactTools/JsvStyleClass";
 function createStyleClass(styleObj) {
   return new JsvTextStyleClass(styleObj).getName() 
 }
-export const block_item = createStyleClass({"fontSize":"30px","height":"100px","width":"100px","backgroundColor":"#000"})
+export const block_item = createStyleClass({"fontSize":"30px","height":"100px","width":"100px","backgroundColor":"#000000"})
 `
 
 export default result
