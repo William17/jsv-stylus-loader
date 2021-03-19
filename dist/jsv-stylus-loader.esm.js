f69e3d44c74cede3bedbbddbcc9a09f005403146
liangweiliang <liangweiliang@coocaa.com>
2021-03-19 10:34:10 +0800

    build:0.1.1

--- dist/jsv-stylus-loader.esm.js
+++ dist/jsv-stylus-loader.esm.js
@@ -2,26 +2,76 @@ import path from 'path';
 import stylus from 'stylus';
 import csstree from 'css-tree';
 
+const jsvPropertyMap = {
+  animation: ["animation", "-webkit-animation"],
+  backgroundImage: ["background-image"],
+  borderRadius: ["border-radius"],
+  borderImage: ["border-image"],
+  borderImageOutset: ["border-image-outset"],
+  borderImageWidth: ["border-image-width"],
+  backgroundColor: ["background-color"],
+  backfaceVisibility: ["backface-visibility", "-webkit-backface-visibility"],
+  clipPath: ["clip-path"],
+  fontFamily: ["font-family"],
+  fontSize: ["font-size"],
+  fontStyle: ["font-style"],
+  lineHeight: ["line-height"],
+  objectFit: ["object-fit"],
+  perspective: ["perspective", "-webkit-perspective"],
+  perspectiveOrigin: ["perspective-origin", "-webkit-perspective-origin"],
+  textAlign: ["text-align"],
+  textOverflow: ["text-overflow"],
+  transform: ["transform", "-webkit-transform"],
+  transformOrigin: ["transform-origin", "-webkit-transform-origin"],
+  transformStyle: ["transform-style", "-webkit-transform-style"],
+  transition: ["transition", "-webkit-transition"],
+  whiteSpace: ["white-space"],
+  zIndex: ["z-index"]
+};
+const propertyMap = Object.keys(jsvPropertyMap).reduce((result, property) => {
+  const cssProperty = jsvPropertyMap[property][0];
+  result[cssProperty] = property;
+  return result;
+}, {});
+function getProperty(cssProperty) {
+  return propertyMap[cssProperty] || cssProperty;
+}
+
 function runtime(classNameMap) {
   let code = `
-    import { JsvStyleClass, JsvTextStyleClass } from "jsview-utils/JsViewReactTools/JsvStyleClass";
-    function createStyleClass(styleObj) {
-      return new JsvTextStyleClass(styleObj).getName() 
-    }
-  `;
+import { JsvTextStyleClass } from "jsview-utils/JsViewReactTools/JsvStyleClass";
+function createStyleClass(styleObj) {
+  return new JsvTextStyleClass(styleObj).getName() 
+}`;
   Object.keys(classNameMap).forEach(item => {
     const node = classNameMap[item];
-    const className = item.replace('.', '').replace(':', '__');
+    const className = item.replace('.', '');
     const styleObj = node.block.children.toArray().reduce((result, styleItem) => {
-      result[styleItem.property] = csstree.generate(styleItem.value);
+      const property = getProperty(styleItem.property);
+      result[property] = csstree.generate(styleItem.value);
       return result;
     }, {});
     code += `
-    export const ${className} = createStyleClass(${JSON.stringify(styleObj)})
-    `;
+export const ${className} = createStyleClass(${JSON.stringify(styleObj)})
+`;
   });
   return code;
 }
+
+const nodes = stylus.nodes;
+const originRGBAToString = nodes.RGBA.prototype.toString;
+
+nodes.RGBA.prototype.toString = function () {
+  // https://github.com/stylus/stylus/issues/1825
+  // six digit color
+  const val = originRGBAToString.call(this);
+
+  if (/^#[A-Za-z0-9]{3}/.test(val)) {
+    return '#' + val[1] + val[1] + val[2] + val[2] + val[3] + val[3];
+  }
+
+  return val;
+};
 
 async function stylusLoader(source) {
   const styl = stylus(source);
@@ -37,45 +87,21 @@ async function stylusLoader(source) {
 
     const ast = csstree.parse(css);
     const classNameMap = {};
-    const focusSelectors = [];
     const selectorTypeMap = {
       'IdSelector': '#',
       'TypeSelector': '',
-      'ClassSelector': '.',
-      'PseudoClassSelector': ':'
+      'ClassSelector': '.'
     };
     csstree.walk(ast, function (node) {
       if (node.type === 'Rule') {
         node.prelude.children.forEach(item => {
           if (item.type === 'Selector') {
-            const info = item.children.reduce((result, selector) => {
-              result.fullName += `${selectorTypeMap[selector.type]}${selector.name}`;
-
-              if (selector.type === 'PseudoClassSelector' && selector.name === 'focus') {
-                result.focus = true;
-              }
-
-              return result;
-            }, {
-              fullName: '',
-              focus: false
-            });
-            classNameMap[info.fullName] = node;
-
-            if (info.focus) {
-              focusSelectors.push(info.fullName);
-            }
+            const fullName = item.children.reduce((result, selector) => {
+              return result + `${selectorTypeMap[selector.type]}${selector.name}`;
+            }, '');
+            classNameMap[fullName] = node;
           }
         });
-      }
-    });
-    focusSelectors.forEach(item => {
-      const styleNode = classNameMap[item.replace(/:focus/, '')];
-      const styleNodeOnFocus = classNameMap[item];
-
-      if (styleNode) {
-        const childrenData = [].concat(styleNode.block.children.toArray()).concat(styleNodeOnFocus.block.children.toArray());
-        styleNodeOnFocus.block.children.fromArray(childrenData);
       }
     });
     callback(null, `${runtime(classNameMap)}`);
